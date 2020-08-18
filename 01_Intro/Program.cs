683983a092df9f73ae6d271da837fad576c9ae20
benvaro91@gmail.com <benvaro91@gmail.com>
2020-08-18 14:10:13 +0300

    Add project files.

---
+++ 01_Intro/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region repeat
+            // ТЗ (на СУБД):
+            // зберігати дані 
+            // формувати звіти 
+            // резервне копіювання і відновлення
+            // система прав користувачів
+            // доступ до даних (по мережі)
+
+            // інтерфейс адміна
+            // API (набір функцій)
+            // універсальна мова - sql
+            // шифрування - захист від несанкціонованого доступу
+
+            // ORM - EntityFramework (EF) 
+            #endregion
+
+            //1) connection string
+            // Data Source - server name
+            //Initial catalog - database
+            // IS - режим аутентифікації, якщо true - windows authentification
+            //                                 false - server authentification
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                        Initial catalog=master;
+                                        Integrated Security=true;";
+            // 2) створюємо об'єкт з'єднання
+            //SqlConnection connection = new SqlConnection();
+            //connection.ConnectionString = connectionString;
+
+            IDbConnection connection = new SqlConnection(connectionString);
+            // 3) відкрити з'єднання
+
+            ((SqlConnection)connection).StateChange += Program_StateChange;
+            #region open and close
+            //try
+            //{
+            //    connection.Open();
+            //    Console.WriteLine($"State: {connection.State}");
+            //    //
+            //    //
+            //}
+            //catch (SqlException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (connection != null)
+            //        connection.Close();
+            //} 
+            #endregion
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine($"State: {connection.State}");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            // code
+        }
+
+        private static void Program_StateChange(object sender, StateChangeEventArgs e)
+        {
+            SqlConnection connection = sender as SqlConnection;
+            Console.WriteLine($"Connection:\n\tDatabase: {connection.Database}" +
+                $"\n\tServer: {connection.DataSource}" +
+                $"\n\tState: {connection.State}");
+        }
+    }
+}
---
+++ 03_params/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine(connection.ConnectionString);
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Enter name of group");
+                string nameGroup = Console.ReadLine();
+
+                SearchStudentsInGroup(nameGroup, connection);
+            }
+
+            #region using SqlConnectionStringBuilder
+            //     SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            //builder.DataSource = tb_ip.Text;
+            //builder.UserID = tb_login.Text;
+
+            //       SqlConnection connection = new SqlConnection(builder.ConnectionString); 
+            #endregion
+            //D - dont
+            //R - repeat
+            //Y - yourself
+
+         
+        }
+
+        private static void SearchStudentsInGroup(string nameGroup, SqlConnection connection)
+        {
+            #region using parameters in command
+            SqlParameter groupParameter = new SqlParameter();
+            groupParameter.ParameterName = "@p1";
+            groupParameter.SqlDbType = System.Data.SqlDbType.NVarChar;
+            groupParameter.Size = 20;
+            groupParameter.Value = nameGroup;
+            #endregion
+
+            SqlCommand command = new SqlCommand($"select Student.Name, Surname, IdGroup from Student join Groups " +
+                $"On Student.IdGroup = Groups.Id Where Groups.Name = @p1;select * from Groups", connection);
+        
+            command.Parameters.Add(groupParameter);
+
+            SqlDataReader reader = command.ExecuteReader();
+            int line = 0;
+            if (reader.HasRows)
+            {
+                do
+                {
+                    line = 0;
+                    while (reader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.Write($"[{reader.GetName(i)}]\t\t");
+                            }
+                        }
+                        Console.WriteLine(Environment.NewLine);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        line++;
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"[{reader.GetValue(i)}]\t\t");
+                        }
+
+                    }
+                    Console.WriteLine();
+                } while (reader.NextResult());
+            }
+        }
+    }
+}
