cbcc9e4c59d74d42f9256f28a6b39f6f008b9b00
benvaro91@gmail.com <benvaro91@gmail.com>
2020-08-20 14:11:15 +0300

    using ProviderFactory

---
+++ 07_ProviderFactory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ProviderFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+            // 1. 
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                Console.WriteLine($"Connection: {connection.GetType().Name}");
+
+                DbCommand command = factory.CreateCommand();
+                command.CommandText = "Select * from Groups";
+                command.Connection = connection;
+
+                Console.WriteLine($"Command: {command.GetType().Name}");
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                Console.WriteLine($"Reader: {reader.GetType().Name}");
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write("{0, 25}", reader[i]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
